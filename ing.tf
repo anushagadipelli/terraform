1d3dd8fff587520abb595148f1711870ec8f550d
Sasanka <37572993+sasanka9@users.noreply.github.com>
2023-06-01 06:14:59 +0530

    Update ing.tf
    added routing

--- ing.tf
+++ ing.tf
@@ -5,3 +5,12 @@ ingress {
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
     }
+
+
+#add routing as well
+resource "aws_route" "update" {
+    provider               = aws.docdb_peer
+    route_table_id         = "${aws_vpc.docdb_peer.default_route_table_id}"
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = "${aws_internet_gateway.gw_connect.id}"
+}
